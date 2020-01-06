2c9051dc27a06c1f85ee48bcbfba06c9dc329b31
Danilo Requena <danilo.requena26@gmail.com>
2020-01-06 16:03:06 -0300

    Initial Commit

---
+++ FrasesChaves/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrasesChaves
+//
+//  Created by Danilo Requena on 06/01/20.
+//  Copyright © 2020 Danilo Requena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
